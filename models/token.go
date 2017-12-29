75e644711a84d2a0d23ed1b07462b009063ccb47
Ildar Usmanov <wajox@mail.ru>
2017-12-29 10:48:40 +0300

    add token implementation

--- models/token.go
+++ models/token.go
@@ -1,10 +1,16 @@
 package models
 
+import (
+    "time"
+)
+
 type Token struct {
     tokenUserId string
+    tokenValue string
+    tokenScope []string
     tokenTimestamp int64
     tokenLifetime int
-    tokenScope []string
+    tokenScopeMap map[string]struct{}
 
     GetTokenUserId() string
     GetTokenTimestamp() int64
@@ -12,4 +18,60 @@ type Token struct {
     GetTokenScope() []string
     IsValid() bool
     InScope([]string) bool
+}
+
+func CreateNewToken(tokenUserId, tokenValue string, tokenScope []string, tokenTimestamp int64, tokenLifetime int) *Token {
+    newToken = &Token{
+        tokenUserId,
+        tokenValue,
+        tokenScope,
+        tokenTimestamp,
+        tokenLifetime,
+    }
+
+    newToken.initScope()
+}
+
+func (t *Token) initScope() {
+    t.tokenScopeMap = make(map[string]struct{}, len(t.tokenScope))
+    
+    for _, s := range t.tokenScope {
+        t.tokenScopeMap[s] = struct{}{}
+    }
+}
+
+func (t *Token) GetTokenUserId() string {
+    return t.tokenUserId
+}
+
+func (t *Token) GetTokenValue() string {
+    return t.tokenValue
+}
+
+func (t *Token) GetTokenTimestamp() int64 {
+    return t.tokenTimestamp
+}
+
+func (t *Token) GetTokenScope() []string {
+    return t.tokenScope
+}
+
+func (t *Token) InScope(needle []string) bool {
+    allInScope := true
+
+    for _, s := range needle {
+        if _, ok := t.tokenScopeMap(s); !ok {
+            allInScope = false
+        }
+    }
+
+    return allInScope
+}
+
+func (t *Token) IsValid() bool {
+    if t.tokenLifetime == 0 {
+        return true
+    }
+
+    return time.Now().Unix() < t.tokenTimestamp + t.tokenLifetime
 }
