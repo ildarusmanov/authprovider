cf173e67fd053d70186912ede67e8c9f0616d9e1
Ildar Usmanov <wajox@mail.ru>
2018-01-09 12:14:57 +0300

    add some red tests for request validator

--- services/request_validator_test.go
+++ services/request_validator_test.go
@@ -2,12 +2,20 @@ package services
 
 import (
 	"testing"
+    "crypto/md5"
+    "time"
 )
 
 const requestValidatorToken = "super-token"
+const anotherRequestValidatorToken = "another-super-token"
+var (
+    currentTime = time.Now().Unix()
+    currentTimeSignature = md5.Sum([]byte(requestValidatorToken + ":" + string(currentTime)))
+    anotherTimeSignature = md5.Sum([]byte("signature-value"))
+)
 
 func TestCreateNewRequestValidator(t *testing.T) {
-	rv := CreateNewRequestValidator(requestValidatorToken)
+    rv := CreateNewRequestValidator(requestValidatorToken)
 
 	if rv == nil {
 		t.Error("CreateNewRequestValidator() returns nil")
@@ -15,5 +23,18 @@ func TestCreateNewRequestValidator(t *testing.T) {
 }
 
 func TestValidate(t *testing.T) {
-	t.Error("test not implemented yet")
+    rvCurrent := CreateNewRequestValidator(requestValidatorToken)
+    rvAnother := CreateNewRequestValidator(anotherRequestValidatorToken)
+
+    if !rvCurrent.Validate(currentTimeSignature, currentTime) {
+        t.Error("Signature must be validated properly")
+    }
+
+    if rvCurrent.Validate(currentTimeSignature, currentTime + 1) {
+        t.Error("Signature must not be validated properly")
+    }
+
+    if rvAnother.Validate(anotherTimeSignature, currentTime) {
+        t.Error("Signature must not be validated")
+    }
 }
