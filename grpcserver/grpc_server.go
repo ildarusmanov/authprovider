8b9726482026bd235c59c90e61dd025c1448afc0
Ildar Usmanov <wajox@mail.ru>
2018-01-10 22:41:28 +0300

    add test for grpc server, add some implementation

--- grpcserver/grpc_server.go
+++ grpcserver/grpc_server.go
@@ -2,26 +2,63 @@ package grpcserver
 
 import (
     "golang.org/x/net/context"
+    "errors"
 )
 
-type GrpcServer struct{}
+var invalidReqSignature = errors.New("signatur not valid")
 
-func CreateNewGrpcServer() *GrpcServer {
-	return &GrpcServer{}
+type requestValidator interface {
+    Validate(signature string, timestamp int64) bool
 }
 
-func (s *GrpcServer) AddToken(context.Context, *TokenRequest) (*TokenResponse, error) {
+type GrpcServer struct{
+    rv requestValidator
+}
+
+func CreateNewGrpcServer(rv requestValidator) *GrpcServer {
+	return &GrpcServer{rv}
+}
+
+func CreateToken(value, userId string, lifetime int32, timestamp int64, scope []string) *Token {
+    return &Token{value, userId, lifetime, timestamp, scope}
+}
+
+func CreateTokenRequest(signature string, timestamp int64, token *Token)  *TokenRequest {
+    return &TokenRequest{signature, timestamp, token}
+}
+
+func CreateTokenResponse(isOk bool, status string, token *Token)  *TokenResponse{
+    return &TokenResponse{isOk, status, token}
+}
+
+func (s *GrpcServer) AddToken(ctx context.Context, r *TokenRequest) (*TokenResponse, error) {
+    if !s.rv.Validate(r.GetSignature(), r.GetTimestamp()) {
+        return nil, invalidReqSignature
+    }
+
     return nil, nil
 }
 
-func (s *GrpcServer) FindToken(context.Context, *TokenRequest) (*TokenResponse, error) {
+func (s *GrpcServer) FindToken(ctx context.Context, r *TokenRequest) (*TokenResponse, error) {
+    if !s.rv.Validate(r.GetSignature(), r.GetTimestamp()) {
+        return nil, invalidReqSignature
+    }
+
     return nil, nil
 }
 
-func (s *GrpcServer) DropToken(context.Context, *TokenRequest) (*TokenResponse, error) {
+func (s *GrpcServer) DropToken(ctx context.Context, r *TokenRequest) (*TokenResponse, error) {
+    if !s.rv.Validate(r.GetSignature(), r.GetTimestamp()) {
+        return nil, invalidReqSignature
+    }
+
     return nil, nil
 }
 
-func (s *GrpcServer) ValidateToken(context.Context, *TokenRequest) (*TokenResponse, error) {
+func (s *GrpcServer) ValidateToken(ctx context.Context, r *TokenRequest) (*TokenResponse, error) {
+    if !s.rv.Validate(r.GetSignature(), r.GetTimestamp()) {
+        return nil, invalidReqSignature
+    }
+
     return nil, nil
 }
--- grpcserver/grpc_server_test.go
+++ grpcserver/grpc_server_test.go
@@ -1,14 +1,49 @@
 package grpcserver
 
 import (
+    "github.com/ildarusmanov/authprovider/services"
 	"github.com/stretchr/testify/assert"
+    "golang.org/x/net/context"
 	"testing"
+    "time"
 	// "google.golang.org/grpc/test/grpc_testing"
 )
 
+const rvToken = "request validator token"
+
 func TestCreateNewServer(t *testing.T) {
-	s := CreateNewGrpcServer()
+    rv := services.CreateNewRequestValidator(rvToken)
+	s := CreateNewGrpcServer(rv)
 
 	assert.NotNil(t, s)
     assert.Implements(t, (*TokenStorageServer)(nil), s)
+}
+
+func TestAddToken(t *testing.T) {
+    assert := assert.New(t)
+
+    token := CreateToken(
+        "",
+        "123",
+        100,
+        time.Now().Unix(),
+        []string{"all"},
+    )
+
+    rv := services.CreateNewRequestValidator(rvToken)
+    s := CreateNewGrpcServer(rv)
+    timestamp := time.Now().Unix()
+    signature := rv.CreateSignature(timestamp)
+
+    validReq := CreateTokenRequest(signature, timestamp, token)
+    invalidReq := CreateTokenRequest("some-sig", timestamp, nil)
+
+    invalidResp, err := s.AddToken(context.Background(), invalidReq)
+    assert.NotNil(err)
+    assert.False(invalidResp.GetIsOk())
+
+    validResp, err := s.AddToken(context.Background(), validReq)
+    assert.Nil(err)
+    assert.True(validResp.GetIsOk())
+    assert.Equal(token.GetUserId(), validResp.GetToken().GetUserId())
 }
