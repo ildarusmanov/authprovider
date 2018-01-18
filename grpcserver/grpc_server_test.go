f49273c35461756e609fc4d05a6d816f866d0489
Ildar Usmanov <wajox@mail.ru>
2018-01-18 16:18:32 +0300

    fix cs with go fmt

--- grpcserver/grpc_server_test.go
+++ grpcserver/grpc_server_test.go
@@ -13,58 +13,58 @@ import (
 const rvToken = "request validator token"
 
 func TestCreateToken(t *testing.T) {
-    var (
-        tValue = "ttt"
-        tUserId = "123"
-        tLifetime = 100
-        tTimestamp = time.Now().Unix()
-        tScope = []string{"all"}
-    )
+	var (
+		tValue     = "ttt"
+		tUserId    = "123"
+		tLifetime  = 100
+		tTimestamp = time.Now().Unix()
+		tScope     = []string{"all"}
+	)
 
-    token := CreateToken(
-        tValue,
-        tUserId,
-        int32(tLifetime),
-        tTimestamp,
-        tScope,
-    )
+	token := CreateToken(
+		tValue,
+		tUserId,
+		int32(tLifetime),
+		tTimestamp,
+		tScope,
+	)
 
-    assert := assert.New(t)
+	assert := assert.New(t)
 
-    if assert.NotNil(token) {
-        assert.Equal(tValue, token.GetValue())
-        assert.Equal(tUserId, token.GetUserId())
-        assert.Equal(tLifetime, token.GetLifetime())
-        assert.Equal(tScope, token.GetScope())
-    }
+	if assert.NotNil(token) {
+		assert.Equal(tValue, token.GetValue())
+		assert.Equal(tUserId, token.GetUserId())
+		assert.Equal(tLifetime, token.GetLifetime())
+		assert.Equal(tScope, token.GetScope())
+	}
 }
 
 func TestCreateTokenResponse(t *testing.T) {
-    var (
-        rIsOk = true
-        rStatus = "status-text"
-        rToken = CreateToken(
-            "ttt",
-            "123",
-            100,
-            time.Now().Unix(),
-            []string{"all"},
-        )
-    )
+	var (
+		rIsOk   = true
+		rStatus = "status-text"
+		rToken  = CreateToken(
+			"ttt",
+			"123",
+			100,
+			time.Now().Unix(),
+			[]string{"all"},
+		)
+	)
 
-    resp := CreateTokenResponse(rIsOk, rStatus, rToken)
+	resp := CreateTokenResponse(rIsOk, rStatus, rToken)
 
-    assert := assert.New(t)
-    assert.Equal(resp.GetIsOk(), rIsOk)
-    assert.Equal(resp.GetStatus(), rStatus)
+	assert := assert.New(t)
+	assert.Equal(resp.GetIsOk(), rIsOk)
+	assert.Equal(resp.GetStatus(), rStatus)
 
-    if assert.NotNil(resp.GetToken()) {
-        assert.Equal(resp.GetToken().GetValue(), rToken.GetValue())
-        assert.Equal(resp.GetToken().GetUserId(), rToken.GetUserId())
-        assert.Equal(resp.GetToken().GetLifetime(), rToken.GetLifetime())
-        assert.Equal(resp.GetToken().GetTimestamp(), rToken.GetTimestamp())
-        assert.Equal(resp.GetToken().GetScope(), rToken.GetScope())
-    }
+	if assert.NotNil(resp.GetToken()) {
+		assert.Equal(resp.GetToken().GetValue(), rToken.GetValue())
+		assert.Equal(resp.GetToken().GetUserId(), rToken.GetUserId())
+		assert.Equal(resp.GetToken().GetLifetime(), rToken.GetLifetime())
+		assert.Equal(resp.GetToken().GetTimestamp(), rToken.GetTimestamp())
+		assert.Equal(resp.GetToken().GetScope(), rToken.GetScope())
+	}
 }
 
 func TestCreateNewServer(t *testing.T) {
@@ -158,104 +158,103 @@ func TestFindToken(t *testing.T) {
 }
 
 func TestDropToken(t *testing.T) {
-    assert := assert.New(t)
+	assert := assert.New(t)
 
-    var (
-        userId1    = "123"
-        userId2    = "134"
-        tokenValue = "1"
-        lifetime   = 100
-        scope      = []string{"all"}
-    )
+	var (
+		userId1    = "123"
+		userId2    = "134"
+		tokenValue = "1"
+		lifetime   = 100
+		scope      = []string{"all"}
+	)
 
-    rv := services.CreateNewRequestValidator(rvToken)
-    p := providers.CreateNewMemoryTokenProvider()
+	rv := services.CreateNewRequestValidator(rvToken)
+	p := providers.CreateNewMemoryTokenProvider()
 
-    token, err := p.AddToken(userId1, scope, lifetime)
+	token, err := p.AddToken(userId1, scope, lifetime)
 
-    assert.Nil(err)
+	assert.Nil(err)
 
-    token1 := CreateToken(
-        token.GetTokenValue(),
-        token.GetTokenUserId(),
-        int32(token.GetTokenLifetime()),
-        token.GetTokenTimestamp(),
-        token.GetTokenScope(),
-    )
+	token1 := CreateToken(
+		token.GetTokenValue(),
+		token.GetTokenUserId(),
+		int32(token.GetTokenLifetime()),
+		token.GetTokenTimestamp(),
+		token.GetTokenScope(),
+	)
 
-    token2 := CreateToken(
-        tokenValue,
-        userId2,
-        int32(lifetime),
-        time.Now().Unix(),
-        scope,
-    )
+	token2 := CreateToken(
+		tokenValue,
+		userId2,
+		int32(lifetime),
+		time.Now().Unix(),
+		scope,
+	)
 
-    s := CreateNewGrpcServer(rv, p)
-    timestamp := time.Now().Unix()
-    signature := rv.CreateSignature(timestamp)
+	s := CreateNewGrpcServer(rv, p)
+	timestamp := time.Now().Unix()
+	signature := rv.CreateSignature(timestamp)
 
-    validReq := CreateTokenRequest(signature, timestamp, token1)
-    invalidReq := CreateTokenRequest(signature, timestamp, token2)
+	validReq := CreateTokenRequest(signature, timestamp, token1)
+	invalidReq := CreateTokenRequest(signature, timestamp, token2)
 
-    invalidResp, err := s.DropToken(context.Background(), invalidReq)
-    assert.NotNil(err)
-    assert.False(invalidResp.GetIsOk())
+	invalidResp, err := s.DropToken(context.Background(), invalidReq)
+	assert.NotNil(err)
+	assert.False(invalidResp.GetIsOk())
 
-    validResp, err := s.DropToken(context.Background(), validReq)
-    assert.Nil(err)
-    assert.True(validResp.GetIsOk())
-    assert.Equal(token1.GetUserId(), validResp.GetToken().GetUserId())
+	validResp, err := s.DropToken(context.Background(), validReq)
+	assert.Nil(err)
+	assert.True(validResp.GetIsOk())
+	assert.Equal(token1.GetUserId(), validResp.GetToken().GetUserId())
 }
 
-
 func TestValidateToken(t *testing.T) {
-    assert := assert.New(t)
+	assert := assert.New(t)
 
-    var (
-        userId1    = "123"
-        userId2    = "123"
-        tokenValue = "1"
-        lifetime   = 100
-        scope      = []string{"all"}
-    )
+	var (
+		userId1    = "123"
+		userId2    = "123"
+		tokenValue = "1"
+		lifetime   = 100
+		scope      = []string{"all"}
+	)
 
-    rv := services.CreateNewRequestValidator(rvToken)
-    p := providers.CreateNewMemoryTokenProvider()
+	rv := services.CreateNewRequestValidator(rvToken)
+	p := providers.CreateNewMemoryTokenProvider()
 
-    token, err := p.AddToken(userId1, scope, lifetime)
+	token, err := p.AddToken(userId1, scope, lifetime)
 
-    assert.Nil(err)
+	assert.Nil(err)
 
-    token1 := CreateToken(
-        token.GetTokenValue(),
-        token.GetTokenUserId(),
-        int32(token.GetTokenLifetime()),
-        token.GetTokenTimestamp(),
-        token.GetTokenScope(),
-    )
+	token1 := CreateToken(
+		token.GetTokenValue(),
+		token.GetTokenUserId(),
+		int32(token.GetTokenLifetime()),
+		token.GetTokenTimestamp(),
+		token.GetTokenScope(),
+	)
 
-    token2 := CreateToken(
-        tokenValue,
-        userId2,
-        int32(lifetime),
-        time.Now().Unix(),
-        scope,
-    )
+	token2 := CreateToken(
+		tokenValue,
+		userId2,
+		int32(lifetime),
+		time.Now().Unix(),
+		scope,
+	)
 
-    s := CreateNewGrpcServer(rv, p)
-    timestamp := time.Now().Unix()
-    signature := rv.CreateSignature(timestamp)
+	s := CreateNewGrpcServer(rv, p)
+	timestamp := time.Now().Unix()
+	signature := rv.CreateSignature(timestamp)
 
-    validReq := CreateTokenRequest(signature, timestamp, token1)
-    invalidReq := CreateTokenRequest(signature, timestamp, token2)
+	validReq := CreateTokenRequest(signature, timestamp, token1)
+	invalidReq := CreateTokenRequest(signature, timestamp, token2)
 
-    invalidResp, err := s.ValidateToken(context.Background(), invalidReq)
-    assert.NotNil(err)
-    assert.False(invalidResp.GetIsOk())
+	invalidResp, err := s.ValidateToken(context.Background(), invalidReq)
+	assert.NotNil(err)
+	assert.False(invalidResp.GetIsOk())
 
-    validResp, err := s.ValidateToken(context.Background(), validReq)
-    assert.Nil(err)
-    assert.True(validResp.GetIsOk())
-    assert.Equal(token1.GetUserId(), validResp.GetToken().GetUserId())
+	validResp, err := s.ValidateToken(context.Background(), validReq)
+	assert.Nil(err)
+	assert.True(validResp.GetIsOk())
+	assert.Equal(token1.GetUserId(), validResp.GetToken().GetUserId())
 }
