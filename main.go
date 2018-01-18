3e1400362748d53d2bbee1f7b51bb2e93db1a05d
Ildar Usmanov <wajox@mail.ru>
2018-01-18 17:16:27 +0300

    implement entrypoint, ad gitignore file

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+    "os"
+    "os/signal"
+    "log"
+)
 
+func main() {
+    log.Printf("[*] Starting")
+    stop := make(chan os.Signal, 1)
+    signal.Notify(stop, os.Interrupt)
+
+    log.Printf("[*] Ready")
+
+    <- stop
+
+    log.Printf("[*] Finished")
 }
